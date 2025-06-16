e4bcc032873011e861114476f3353153d14cc876
Alexander Vowinkel <alexander.vowinkel@bayer.com>
2025-06-16 13:06:50 +0200

    clear fe ontology cache when version suffix changed

--- frontend/src/versionCheck.ts
+++ frontend/src/versionCheck.ts
@@ -16,20 +16,43 @@ export const versionCheck = async () => {
   xmlHttp.send(null)
   const response = JSON.parse(xmlHttp.responseText)
   if (response.status === 'ok') {
-    const storedVersions: StoredVersion | null = await localforage.getItem('__v')
+    const storedVersions: StoredVersion = (await localforage.getItem('__v')) || {b: '', f: ''}
     const backendVersion = response.version
     const frontendVersion = COMMIT_HASH.trim()
 
-    console.log(`Backend version was ${storedVersions?.b} and is now ${backendVersion}`)
-    console.log(`Frontend version was ${storedVersions?.f} and is now ${frontendVersion}`)
+    const versionSuffix = versionSuffixOf(backendVersion)
+    const storedVersionSuffix = versionSuffixOf(storedVersions.b)
 
-    if (!storedVersions || storedVersions.b !== backendVersion || storedVersions.f !== frontendVersion) {
+    console.log(`Backend version was ${storedVersions.b} and is now ${backendVersion}`)
+    console.log(`Frontend version was ${storedVersions.f} and is now ${frontendVersion}`)
+
+    if (
+      !storedVersions.b ||
+      !storedVersions.f ||
+      storedVersions.b !== backendVersion ||
+      storedVersions.f !== frontendVersion
+    ) {
       console.log('Clearing local cache')
+      console.time('Clearing local cache')
       upgraded = true
       await localforage.clear()
       await localforage.setItem('__v', {b: backendVersion, f: frontendVersion})
+      console.timeEnd('Clearing local cache')
+
+      if (versionSuffix != storedVersionSuffix) {
+        console.log('Clearing local ontology cache')
+        console.time('Clearing local ontology cache')
+        await db.codes.clear()
+        await db.ontologies.clear()
+        console.timeEnd('Clearing local ontology cache')
+      }
     }
   }
 
   return upgraded
+}
+
+const versionSuffixOf = (version: string) => {
+  const dashIndex = version.indexOf('-')
+  return dashIndex !== -1 ? version.substring(dashIndex + 1) : ''
 }
